830ab74b0d7075fbc9e55209e78c4d2752c7ae0f
Justin <justakinyi1@gmail.com>
2022-05-31 12:54:47 -0700

    prints the ASCII alphabet, in lowercase, not followed by a new line

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = number % 10
+char = Last digit of {number :d} is " + num + " ")
+if (num > 5):
+    print (char
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+i in range('a','z')
+print(f{i :s})

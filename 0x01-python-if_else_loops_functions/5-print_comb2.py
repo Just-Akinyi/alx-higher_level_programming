5c469990753ad5438e1b1ae1c6934a0d08e00b87
Justin <justakinyi1@gmail.com>
2022-05-31 15:43:24 -0700

    program that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for digit in range(0, 10):
-    for digit1 in range(0,10):
-        print("{}{}".format(digit, digit1), end=", "
-                if (digit and digit1 == 9)
-                print("{}{}".format(digit, digit1))
+for digit in range(0, 99):
+        print("{}".format(digit), end=", ")

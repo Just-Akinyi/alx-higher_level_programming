2d52de245e56db3ae744b8496c6414ae3340607a
Justin <justakinyi1@gmail.com>
2022-06-04 17:12:38 +0300

    prints 0 to 9

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for digit in range(0, 99):
-    print("{}".format(digit), end=", ")
+for digiti1 in range(0, 9):
+    for digit2 in range(0, 9):
+        print("{}{}".format(digit1, digit2), end=", ")

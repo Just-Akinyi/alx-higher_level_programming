a09bfe613fbcce34e3151f2b19cdf6771247475d
Justin <justakinyi1@gmail.com>
2022-06-04 17:51:33 +0300

    prints numbers, precision of 0 for one digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for digit in range(0, 100):
     if (digit == 99):
         print("{}".format(digit))
     else:
-        print("{:02}".format(digit), end=", ")    i
+        print("{:02}".format(digit), end=", ")

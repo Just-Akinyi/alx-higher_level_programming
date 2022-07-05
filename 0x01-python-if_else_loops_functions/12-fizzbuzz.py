d71c2c82337cb865edc6d22c780e9d3b0734681e
Justin <justakinyi1@gmail.com>
2022-07-05 16:54:27 +0300

    FizzBuzz Challenge

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(1, 101):
     if(i % 3 == 0 and i % 5 == 0):
-        print("fizzbuzz", end=" ")
+        print("FizzBuzz", end=" ")
     elif(i % 3 == 0):
         print("Fizz", end=" ")
     elif(i % 5 == 0):

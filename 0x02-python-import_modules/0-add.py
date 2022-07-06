73c9f8714eecdf4406d18c6bb98f8d8494169cc1
Justin <justakinyi1@gmail.com>
2022-07-06 06:46:15 +0300

    program that imports a function and prints the result of addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    x = add(a, b) 
-    print("{:d} + {:d} = {:d}".format(a, b, x)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

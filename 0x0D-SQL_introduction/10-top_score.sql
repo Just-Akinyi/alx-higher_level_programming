fff3e2c168cbc9b5a64b975308b68caaec6817e1
Justin Akinyi <justakinyi1@gmail.com>
2022-08-11 12:23:35 +0300

    Lists all records of the table second_table Records are ordered by descending score.

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
--- Lists all records of the table second_table.
+-- Lists all records of the table second_table
 -- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`

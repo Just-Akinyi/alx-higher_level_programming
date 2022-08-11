ae749a0da0b4d4c1b3eef092dff654de7b0e42fb
Justin Akinyi <justakinyi1@gmail.com>
2022-08-11 12:32:10 +0300

    Lists all records of the table second_table having a name value in descending score. SELECT `score`, `name`

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- Lists all records of the table second_table having a name value.
+-- Lists all records of the table second_table having a name value
 -- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`

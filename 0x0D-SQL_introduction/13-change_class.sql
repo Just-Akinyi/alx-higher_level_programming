a61ef2a476a24c29526af9cd41f707ca100cedad
Justin Akinyi <justakinyi1@gmail.com>
2022-08-11 12:27:50 +0300

    Removes all records with a score <= 5 in the table second_table.

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
--- Removes all records with a score <= 5 in the table second_table.
+-- Removes all records with a score <= 5 in the table second_table
 DELETE FROM `second_table`
 WHERE `score` <= 5;

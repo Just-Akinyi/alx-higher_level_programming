dd98508d9545256712b948522fa6c4edbd82aaf1
Justin Akinyi <justakinyi1@gmail.com>
2022-08-12 19:46:56 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.cities` (
     `state_id` INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
     FOREIGN KEY(`state_id`)
-    REFERENCES `hbtn_0d_usa.states`(`id`)
+    REFERENCES `states`(`id`)
 );

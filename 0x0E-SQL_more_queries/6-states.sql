369177fc30d01843858edd86bd9bba370988fc2f
Justin Akinyi <justakinyi1@gmail.com>
2022-08-12 19:40:17 +0300

    Creates the table hbtn_0d_usa with table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(`id`),
-    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );

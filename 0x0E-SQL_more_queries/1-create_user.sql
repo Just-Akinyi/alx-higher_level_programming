8aeaf696051f903d00dcf3b65b4cd11bfa39cfb7
Justin Akinyi <justakinyi1@gmail.com>
2022-08-12 19:23:06 +0300

    Creates the user user_0d_1 with all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Creates the user user_0d_1 with all privileges.
+-- Creates the user user_0d_1 with all privileges
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';

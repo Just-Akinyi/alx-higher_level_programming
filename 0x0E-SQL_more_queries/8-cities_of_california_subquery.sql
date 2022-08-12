caeab03e3a7bcfbd5452d555876f7d776fe7b8a3
Justin Akinyi <justakinyi1@gmail.com>
2022-08-12 19:55:29 +0300

    Lists all cities of CA in the database hbtn_0d_usa

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
--- Lists all cities of CA in the database hbtn_0d_usa.
+-- Lists all cities of CA in the database hbtn_0d_usa
 -- Results are ordered by ascending cities.id.
 SELECT `id`, `name`
   FROM `cities`

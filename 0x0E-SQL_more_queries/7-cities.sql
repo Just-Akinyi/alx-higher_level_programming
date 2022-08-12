f4fd3327e5fb8fcd20f200db9c8642124d00ca49
Justin Akinyi <justakinyi1@gmail.com>
2022-08-12 19:50:05 +0300

    creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
--- Creates the database hbtn_0d_usa with the table cities.
+-- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.cities` (
-    PRIMARY KEY(`id`),
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `state_id` INT NOT NULL,
-    `name` VARCHAR(256) NOT NULL,
-    FOREIGN KEY(`state_id`)
-    REFERENCES `states`(`id`)
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `cities`
+(
+  `id` INT NOT NULL UNIQUE AUTO_INCREMENT,
+  `state_id` INT NOT NULL,
+  `name` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
 );

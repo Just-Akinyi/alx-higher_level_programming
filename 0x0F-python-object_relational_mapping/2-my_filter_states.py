54cb430ec10bb292ff9cc003cf77ce97d2744646
Just-Akinyi <justakinyi1@gmail.com>
2022-09-10 15:57:34 +0300

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in the states table of hbtn_0e_0_usa 
+takes in an argument and displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument
 """
 from sys import argv

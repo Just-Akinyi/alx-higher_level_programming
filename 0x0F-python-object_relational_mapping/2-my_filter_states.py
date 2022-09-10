9c8cc1a388284f32dbc55757e36f728c6920f73d
Just-Akinyi <justakinyi1@gmail.com>
2022-09-10 14:15:35 +0300

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
 """
 from sys import argv
 import MySQLdb

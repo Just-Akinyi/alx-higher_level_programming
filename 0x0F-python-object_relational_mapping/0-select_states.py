33ed6a4b6637479ffaa1160fc708f5b54b95b402
Just-Akinyi <justakinyi1@gmail.com>
2022-09-10 13:37:20 +0300

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,8 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd= argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     for state in cur.fetchall():
-        print (state)
-        
+        print(state)

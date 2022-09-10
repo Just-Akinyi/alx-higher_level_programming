313e6acd0acfd5f023cc25882cb1059ea9fcba9f
Just-Akinyi <justakinyi1@gmail.com>
2022-09-10 15:50:03 +0300

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Lists all values in the states tables of a database where name
-matches the argument in a safe way
+takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa 
+where name matches the argument
 """
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3]
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
     allstates = cur.fetchall()

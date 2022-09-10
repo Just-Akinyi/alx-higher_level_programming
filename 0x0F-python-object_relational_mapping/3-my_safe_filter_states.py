1e0192d588b89cfb0efe23b1c30603b0847da3a2
Just-Akinyi <justakinyi1@gmail.com>
2022-09-10 20:32:06 +0300

    takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument(safe from MySQL injections!)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,3 +4,14 @@ takes in arguments and displays
 all values in the states table of hbtn_0e_0_usa
 where name matches the argument(safe from MySQL injections!)
 '''
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    allstates = cur.fetchall()
+    for state in allstates:
+        if state[1] == argv[4]:
+            print(state)

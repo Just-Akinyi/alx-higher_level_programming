29be89a845cd6661aabd119bbcdc41dd995fa41e
Just-Akinyi <justakinyi1@gmail.com>
2022-09-10 14:05:39 +0300

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (argv[4],))
+    allstates = cur.fetchall()
+    for state in allstates:
+        print(state)

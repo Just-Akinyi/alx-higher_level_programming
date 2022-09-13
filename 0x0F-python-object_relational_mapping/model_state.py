cebd9cdd5a67be23afba49ed6ed676a7cf35ec5b
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 15:22:03 +0300

    contains the class definition of a State and an instance Base = declarative_base():

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,9 @@ Contains State class and Base, an instance of declarative_base()
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
     """
@@ -13,5 +15,4 @@ class State(Base):
     """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name =  Column(String(128), nullable=False)
-    
+    name = Column(String(128), nullable=False)

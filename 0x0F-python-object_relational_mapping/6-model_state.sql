663ef7a73023fa1061705ee520d813d4010750fa
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 15:01:09 +0300

    sql

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -2,7 +2,3 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
 SHOW CREATE TABLE states;
-
-guillaume@ubuntu:~/0x0F$ cat 6-model_state.sql | mysql -uroot -p
-Enter password: 
-ERROR 1146 (42S02) at line 4: Table 'hbtn_0e_6_usa.states' doesn't exist

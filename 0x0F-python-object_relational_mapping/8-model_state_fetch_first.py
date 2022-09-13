cfabb9a9f8a1913e256805bce610a33162fdee8b
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 15:55:19 +0300

    lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,12 +9,12 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'\
-        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State):
-        print("{}: {}".format(state.id, state.name))
+        if state.id == 1:
+            print("{}: {}".format(state.id, state.name))

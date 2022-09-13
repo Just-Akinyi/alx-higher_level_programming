78e8101cb2c3a156da3a5a79d22f08236c164947
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 16:00:14 +0300

    prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-prints the first State object from the database hbtn_0e_6_us
+lists all State objects from the database hbtn_0e_6_usa
 """
 from model_state import Base, State
 from sqlalchemy import (create_engine)

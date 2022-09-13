795170532a45cb07d905346ea2886f907626d533
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 20:22:34 +0300

    d

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-lists all State objects from the database hbtn_0e_6_usa
+changes the name of a State object from the database hbtn_0e_6_usa
 """
 from model_state import Base, State
 from sqlalchemy import (create_engine)
@@ -16,6 +16,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = session.query(State).filter(State.id == 2)
-    new_state.name = "New Mexico"
-    session.commit()
+    new_state = session.query(State).filter(State.id == 2).first()
+    
+    if new_state:
+        new_state.name = "New Mexico"
+        session.commit()

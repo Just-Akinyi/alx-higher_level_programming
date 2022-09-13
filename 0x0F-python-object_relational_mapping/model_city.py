f371db15472e1632a034412b47f66e705f2db0a9
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 22:00:37 +0300

    prints all City objects from the database

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,9 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 class City(Base):
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)

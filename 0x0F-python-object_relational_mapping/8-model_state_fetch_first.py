8080372a4f68166c23b7800b913b9fe4c3db7fd1
Just-Akinyi <justakinyi1@gmail.com>
2022-09-13 16:27:15 +0300

    prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,43 @@
 #!/usr/bin/python3
 """
-prints the first State object from the database hbtn_0e_6_us
+Prints the first State object from the database hbtn_0e_6_usa
 """
+import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sys import argv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State). \
-            filter(State.id == 1):
-        print("{}: {}".format(state.id, state.name))
-        if state == None:
-            print ("Nothing")
+    state = session.query(State).order_by(State.id).first()
+
+    print("Nothing" if not state else "{}: {}".format(state.id, state.name))
+# #!/usr/bin/python3
+# """
+# prints the first State object from the database hbtn_0e_6_us
+# """
+# from model_state import Base, State
+# from sqlalchemy import (create_engine)
+# from sys import argv
+# from sqlalchemy.orm import sessionmaker
+
+# if __name__ == "__main__":
+#     engine = create_engine(
+#         'mysql+mysqldb://{}:{}@localhost/{}'
+#         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+#     Base.metadata.create_all(engine)
+
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     for state in session.query(State). \
+#             filter(State.id == 1):
+#         print("{}: {}".format(state.id, state.name))
+#         if state == None:
+#             print ("Nothing")

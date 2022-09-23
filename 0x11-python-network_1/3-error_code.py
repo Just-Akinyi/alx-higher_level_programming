af19c708aa1d3475bab9d571540b0b9712e1499b
Just-Akinyi <justakinyi1@gmail.com>
2022-09-23 18:13:58 +0300

    takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+takes in a URL,
+sends a request to the URL
+displays the body of the response (decoded in utf-8).
+'''
+from urllib.error import HTTPError
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from sys import argv
+
+if __name__ == "__main__":
+    myUrl = argv[1]
+    req = Request(myUrl)
+    try:
+        urlopen(req)
+    except HTTPError as err:
+        print("Error code: {}".format(err.code))

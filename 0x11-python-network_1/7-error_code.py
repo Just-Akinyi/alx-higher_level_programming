e673afb47b27522b56f86151eb1b055d025453c7
Just111 <justakinyi1@gmail.com>
2022-09-23 09:25:55 -0700

    just like number 3

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,13 +4,13 @@ takes in a URL,
 sends a request to the URL
 displays the body of the response (decoded in utf-8).
 '''
-from requests
+import requests
 from sys import argv
 
 if __name__ == "__main__":
     myUrl = argv[1]
     req = requests.get(myUrl)
-    if req.status_code > 400
-        print("Error code: {}".format(err.code))
+    if req.status_code > 400:
+        print("Error code: {}".format(req.status_code))
     else:    
-        print(res.text)
+        print(req.text)

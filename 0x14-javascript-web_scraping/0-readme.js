c1eed30c164b064e51c91e4a13ccfba01e0c1600
Just-Akinyi <justakinyi1@gmail.com>
2022-10-19 01:05:02 +0300

    writes a string to a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-fs = require('fs');
-myfile = process.argv[2];
-fs.readFile(myfile, 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  return console.log(data);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });

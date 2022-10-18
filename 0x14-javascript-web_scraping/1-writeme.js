14e86b24e10e5db44b9749b19cdd219c4d3fb6cb
Just-Akinyi <justakinyi1@gmail.com>
2022-10-19 00:13:55 +0300

    script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const myfile = process.argv[2];
 fs.readFile(myfile, 'utf8', function (err, data) {
   if (err) {
-    return console.log(err);
-  }
-  return console.log(data);
-});
+    console.log(err);
+  }else{
+  console.log(data);
+}});

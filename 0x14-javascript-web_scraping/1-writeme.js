c1daf50c3e97918480bbdcc099bc403e016aed1f
Just-Akinyi <justakinyi1@gmail.com>
2022-10-19 00:37:16 +0300

    writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });
